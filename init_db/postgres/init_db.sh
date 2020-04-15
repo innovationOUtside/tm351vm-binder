4f88fd8710133c23bfb096f50070541135943859
Alistair Willis <undercertainty@gmail.com>
2020-04-15 12:44:41 +0100

    Added users/passwords and dbs for compatibility with pre-20J notebooks

--- init_db/postgres/init_db.sh
+++ init_db/postgres/init_db.sh
@@ -5,7 +5,17 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #Demo PostgreSQL Database initialisation
 psql postgres -c "CREATE USER testuser PASSWORD 'testpass'"
+
+# Add users for compatibility with pre-20J notebooks
+psql postgres -c "CREATE USER tm351 PASSWORD 'tm351'"
+psql postgres -c "CREATE USER tm351_student PASSWORD 'tm351_pwd'"
+
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
 createdb -O testuser testdb
+
+# Add a database for compatibility with pre-20J notebooks
+createdb -O tm351 tm351
+createdb -O tm351_student tm351_clean
+createdb -O tm351_student tm351_hospital
 
 psql -d testdb -U testuser -f $THISDIR/seed_db.sql
