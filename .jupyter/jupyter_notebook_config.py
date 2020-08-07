e8b72dc9ede5f3dc75b599beefa2d03dd4a6ffed
Tony Hirst <tony.hirst@gmail.com>
2020-08-07 15:51:19 +0100

    Update jupyter_notebook_config.py

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,9 +1,13 @@
 # Traitlet configuration file for jupyter-notebook.
 from os import environ
 
+# TO DO: this should be loaded in from a file (.openrefine_version)
+# Or we change the path to a neutral one?
+OPENREFINE_VERSION=3.3
+
 c.ServerProxy.servers = {
     'openrefine': {
-        'command': [f'{environ["CONDA_DIR"]}/openrefine/openrefine-2.8/refine', '-p', '{port}','-d',f'{environ["HOME"]}/openrefine'],
+        'command': [f'{environ["CONDA_DIR"]}/openrefine/openrefine-{OPENREFINE_VERSION}/refine', '-p', '{port}','-d',f'{environ["HOME"]}/openrefine'],
         'port': 3333,
         'timeout': 120,
         'launcher_entry': {
